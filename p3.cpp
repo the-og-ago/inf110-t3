8cc5b830d6b02488b9b68c008e046b1c03dd0edb
dylan adriel <dylanadriel25@gmail.com>
2026-06-11 08:37:38 -0300

    add fantasma perseguidor

--- p3.cpp
+++ p3.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <random>
+#include <cmath>
 
 using namespace std;
 
@@ -12,28 +13,39 @@ using namespace std;
 // Intenção de movimento: não implementado
 // Atualizado para 3.1.0: André Gustavo   03/06/26
 
-char mapa[11][21] = { // Mapa do jogo
-    "12111111111111111121",
-    "12122222222222222221",
-    "12121121110111212121",
-    "12121121220221211121",
-    "22222220000000222222",
-    "11112121220221211121",
-    "11112121110111212121",
-    "12222222222222222221",
-    "12121112111112112121",
-    "12122222222222222121",
-    "12111111111111111121"};
+// O sistema de perseguicao esta um pouco "defeituoso", as vezes o fantasma fica preso em um loop, indo pra frente e pra tras, acho q isso tem a ver com o mapa ter esquinas e caminhos sem saida, e a forma q o fantasma calcula a melhor direcao
+
+const int MAPLARG = 23;
+const int MAPALT = 13;
+char mapa[MAPALT][MAPLARG] = { // Mapa do jogo
+    "333333333333333333333",
+    "3121111111111111111213",
+    "3121222222222222222213",
+    "3121211211101112121213",
+    "3121211212202212111213",
+    "3222222200000002222223",
+    "3111121212202212111213",
+    "3111121211101112121213",
+    "3122222222222222222213",
+    "3121211121111121121213",
+    "3121222222222222221213",
+    "3121111111111111111213",
+    "3333333333333333333333"};
 
 const float SIZE = 50; // Tamanho de cada célula do mapa
-// posições dos fantasmas
-int gatosx[] = {8, 12, 8, 12};
-int gatosy[] = {3, 3, 5, 5};
+// posições dos fantasmas / o ultimo eh o "inteligente"
+int gatosx[] = {8, 12, 8, 12, 10};
+int gatosy[] = {3, 3, 5, 5, 4};
 
-bool fcima[] = {true, false, false, false};
-bool fbaixo[] = {false, true, false, false};
-bool fesq[] = {false, false, true, false};
-bool fdir[] = {false, false, false, true};
+// direcoes dos fantasmas
+bool fcima[] = {true, false, false, false, false};
+bool fbaixo[] = {false, true, false, false, false};
+bool fesq[] = {false, false, true, false, false};
+bool fdir[] = {false, false, false, true, false};
+
+// target (no jogo sera o pacman, aqui eh o fantasma 2)
+int targetx = gatosx[1];
+int targety = gatosy[1];
 
 // Função para verificar se há mais de 2 caminhos (o que ele veio + novas opções)
 bool ehEncruzilhada(int i)
@@ -98,10 +110,64 @@ void mudarDirecao(int i)
             fdir[i] = true;
     }
 }
+
+void mudarDirecaoInteligente(int i)
+{
+    double dist = 9999;
+    int escolha = 0;
+    if (mapa[gatosy[i] - 1][gatosx[i]] != '1' && (!fbaixo[i] || semSaida(i))) // cima
+    {
+        double d = sqrt(pow(gatosx[4] - targetx, 2) - pow(gatosy[4] - 1 - targety, 2));
+        if (d < dist)
+        {
+            dist = d;
+            escolha = 0;
+        }
+    }
+    else if (mapa[gatosy[i] + 1][gatosx[i]] != '1' && (!fcima[i] || semSaida(i))) // baixo
+    {
+        double d = sqrt(pow(gatosx[4] - targetx, 2) - pow(gatosy[4] + 1 - targety, 2));
+        if (d < dist)
+        {
+            dist = d;
+            escolha = 0;
+        }
+    }
+    if (mapa[gatosy[i]][gatosx[i] - 1] != '1' && (!fdir[i] || semSaida(i))) // esq
+    {
+        float d = sqrt(pow(gatosx[4] - 1 - targetx, 2) - pow(gatosy[4] - targety, 2));
+        if (d < dist)
+        {
+            dist = d;
+            escolha = 0;
+        }
+    }
+    if (mapa[gatosy[i]][gatosx[i] + 1] != '1' && (!fesq[i] || semSaida(i))) // dir
+    {
+        float d = sqrt(pow(gatosx[4] + 1 - targetx, 2) - pow(gatosy[4] - targety, 2));
+        if (d < dist)
+        {
+            dist = d;
+            escolha = 0;
+        }
+    }
+
+    // Reseta todas e ativa a escolhida (nesse caso eh a q tiver a menor distancia direta com o alvo)
+    fcima[i] = fbaixo[i] = fesq[i] = fdir[i] = false;
+    if (escolha == 0)
+        fcima[i] = true;
+    else if (escolha == 1)
+        fbaixo[i] = true;
+    else if (escolha == 2)
+        fesq[i] = true;
+    else if (escolha == 3)
+        fdir[i] = true;
+}
+
 int main()
 {
     // cria a janela
-    sf::RenderWindow window(sf::VideoMode({1000, 550}), "Pac-Man");
+    sf::RenderWindow window(sf::VideoMode({1100, 650}), "Pac-Man");
 
     // cria um quadrado de tamanho 50 (a parede)
     sf::RectangleShape quad({SIZE, SIZE});
@@ -111,6 +177,10 @@ int main()
 
     sf::CircleShape circ(5);
     circ.setFillColor(sf::Color(255, 255, 0));
+
+    // quadrado para as margens do jogo
+    sf::RectangleShape quadmargem({SIZE, SIZE});
+    quadmargem.setFillColor(sf::Color(0, 20, 60));
 
     // sprites do fantasma
     sf::Texture texturegato;
@@ -137,7 +207,7 @@ int main()
         }
 
         // Muda a posição dos fantasmas a cada 0.2 segundos
-        if (fclock.getElapsedTime() > sf::seconds(0.1))
+        if (fclock.getElapsedTime() > sf::seconds(0.2))
         {
             fclock.restart();
             for (int i = 0; i < 4; i++) // para cada fantasma
@@ -155,8 +225,8 @@ int main()
                     {
                         mudarDirecao(i);
                     }
-                    if (gatosy[i] < 0)
-                        gatosy[i] = 10;
+                    if (gatosy[i] < 1)
+                        gatosy[i] = MAPALT - 2;
                 }
                 else if (fbaixo[i])
                 {
@@ -166,8 +236,8 @@ int main()
                     {
                         mudarDirecao(i);
                     }
-                    if (gatosy[i] > 10)
-                        gatosy[i] = 0;
+                    if (gatosy[i] > MAPALT - 2)
+                        gatosy[i] = 1;
                 }
                 else if (fesq[i])
                 {
@@ -177,8 +247,8 @@ int main()
                     {
                         mudarDirecao(i);
                     }
-                    if (gatosx[i] < 0)
-                        gatosx[i] = 19;
+                    if (gatosx[i] < 1)
+                        gatosx[i] = MAPLARG - 3;
                 }
                 else if (fdir[i])
                 {
@@ -188,9 +258,82 @@ int main()
                     {
                         mudarDirecao(i);
                     }
-                    if (gatosx[i] > 19)
-                        gatosx[i] = 0;
+                    if (gatosx[i] > MAPLARG - 3)
+                        gatosx[i] = 1;
                 }
+            }
+
+            // movimentacao do fantasma "inteligente"
+            // para cada direcao livre, ele escolhe a que esta mais perto do alvo, em uma linha reta
+            double menordistancia = 999;
+            double distancia;
+            int direcaoescolhida = -1;
+            if (mapa[gatosy[4] - 1][gatosx[4]] != '1')
+            {
+                distancia = sqrt(pow(gatosx[4] - gatosx[1], 2) + pow(gatosy[4] - 1 - gatosy[1], 2));
+                if (distancia < menordistancia)
+                {
+                    menordistancia = distancia;
+                    direcaoescolhida = 0;
+                }
+            }
+            if (mapa[gatosy[4] + 1][gatosx[4]] != '1')
+            {
+                distancia = sqrt(pow(gatosx[4] - gatosx[1], 2) + pow(gatosy[4] + 1 - gatosy[1], 2));
+                if (distancia < menordistancia)
+                {
+                    menordistancia = distancia;
+                    direcaoescolhida = 1;
+                }
+            }
+            if (mapa[gatosy[4]][gatosx[4] - 1] != '1')
+            {
+                distancia = sqrt(pow(gatosx[4] - 1 - gatosx[1], 2) + pow(gatosy[4] - gatosy[1], 2));
+                if (distancia < menordistancia)
+                {
+                    menordistancia = distancia;
+                    direcaoescolhida = 2;
+                }
+            }
+            if (mapa[gatosy[4]][gatosx[4] + 1] != '1')
+            {
+                distancia = sqrt(pow(gatosx[4] + 1 - gatosx[1], 2) + pow(gatosy[4] - gatosy[1], 2));
+                if (distancia < menordistancia)
+                {
+                    menordistancia = distancia;
+                    direcaoescolhida = 3;
+                }
+            }
+
+            // anda na direcao escolhida
+            if (direcaoescolhida == 0)
+            {
+                if (gatosy[4] < 1)
+                    gatosy[4] = MAPALT - 2;
+                else
+                    gatosy[4]--;
+            }
+            else if (direcaoescolhida == 1)
+            {
+                if (gatosy[4] > MAPALT - 2)
+                    gatosy[4] = 1;
+                else
+                    gatosy[4]++;
+            }
+            else if (direcaoescolhida == 2)
+            {
+
+                if (gatosx[4] < 1)
+                    gatosx[4] = MAPLARG - 3;
+                else
+                    gatosx[4]--;
+            }
+            else if (direcaoescolhida == 3)
+            {
+                if (gatosx[4] > MAPLARG - 3)
+                    gatosx[4] = 1;
+                else
+                    gatosx[4]++;
             }
         }
 
@@ -200,16 +343,26 @@ int main()
         // desenhar tudo aqui...
 
         // desenha paredes
-        for (int i = 0; i < 11; i++)
-            for (int j = 0; j < 21; j++)
+        for (int i = 0; i < 13; i++)
+            for (int j = 0; j < 23; j++)
                 if (mapa[i][j] == '1')
                 {
                     quad.setPosition({j * SIZE, i * SIZE});
                     window.draw(quad);
                 }
 
-        for (int i = 0; i < 11; i++)
-            for (int j = 0; j < 21; j++)
+        // desenha as margens
+        for (int i = 0; i < 13; i++)
+            for (int j = 0; j < 23; j++)
+                if (mapa[i][j] == '3')
+                {
+                    quadmargem.setPosition({j * SIZE, i * SIZE});
+                    window.draw(quadmargem);
+                }
+
+        // desenha pilulas
+        for (int i = 0; i < 13; i++)
+            for (int j = 0; j < 23; j++)
                 if (mapa[i][j] == '2')
                 {
                     circ.setPosition({j * SIZE + 25, i * SIZE + 25});
@@ -222,6 +375,10 @@ int main()
             sprite2.setPosition({gatosx[i] * SIZE, gatosy[i] * SIZE});
             window.draw(sprite2);
         }
+
+        // estou deixando o fantasma que persegue sendo um quadrado de parede, para ser mais visivel nos testes
+        quad.setPosition({gatosx[4] * SIZE, gatosy[4] * SIZE});
+        window.draw(quad);
 
         // termina e desenha o frame corrente
         window.display();
